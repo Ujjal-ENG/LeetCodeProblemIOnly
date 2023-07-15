536271a19496b45ca12795d54c08d07fdcdb8216
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-07-16 00:20:19 +0600

    tried

--- test.js
+++ test.js
@@ -1,14 +1,8 @@
-var kthFactor = function(n, k) {
-  let result = [];
-  for (let i = 1; i <= n; i++){
-    if (n % i === 0) {
-      result.push(i)
-    }
+var maxProfit = function (prices) {
+  let maximumProfit = 0;
+  for (let i = 0; i < prices.length; i++){
+    if(i)
   }
-  if (result.length < k) {
-    return -1
-  }
-  return result[k-1]
 };
 
-console.log(kthFactor(7,2))
+console.log(maxProfit([7,1,5,3,6,4]))
