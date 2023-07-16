ca6ef673f3c15fb437e290ae40ec2e37eeb9d152
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-07-17 00:59:42 +0600

    solved this problem using One pass algorithm now its very optimal from previous one

--- LeetCodeProblem/121Problem.js
+++ LeetCodeProblem/121Problem.js
@@ -1,26 +1,40 @@
 // solved the problem brute force way
+// var maxProfit = function (prices) {
+//   let maximumProfit = 0;
+//   let buyPrice = prices[0];
+//   let noProfit = 0;
+//   for (let i = 0; i < prices.length; i++){
+//     if (prices[i] > (prices[i + 1] === undefined ? 0 : prices[i + 1])) {
+//       noProfit++
+//     }
+//   }
+//   if(noProfit === prices.length) {
+//     return 0
+//   }
+//   for (let i = 1; i < prices.length; i++){
+//     if (prices[i] < buyPrice) {
+//       buyPrice = prices[i]
+//     }
+//     else {
+//      maximumProfit = Math.max(prices[i]-buyPrice,maximumProfit)
+//     }
+//   }
+
+//   return maximumProfit
+// };
+
+// solved this problem optimal way
 var maxProfit = function (prices) {
-  let maximumProfit = 0;
-  let buyPrice = prices[0];
-  let noProfit = 0;
-  for (let i = 0; i < prices.length; i++){
-    if (prices[i] > (prices[i + 1] === undefined ? 0 : prices[i + 1])) {
-      noProfit++
-    }
-  }
-  if(noProfit === prices.length) {
-    return 0
-  }
-  for (let i = 1; i < prices.length; i++){
+  let buyPrice = Infinity;
+  let maxPrice = 0;
+  for (let i = 0; i < prices.length; i++) { 
     if (prices[i] < buyPrice) {
       buyPrice = prices[i]
     }
-    else {
-     maximumProfit = Math.max(prices[i]-buyPrice,maximumProfit)
+    else if (prices[i] - buyPrice > maxPrice) {
+      maxPrice = prices[i]-buyPrice
     }
   }
-
-  return maximumProfit
-};
-
+  return maxPrice
+}
 console.log(maxProfit([7,1,5,3,6,4]))
