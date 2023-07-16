4fcdf5131309912b8d85813413e62bc9a0a6e8b6
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-07-17 00:58:50 +0600

    solved this problem again

--- test.js
+++ test.js
@@ -1,25 +1,13 @@
 var maxProfit = function (prices) {
-  let maximumProfit = 0;
-  let buyPrice = prices[0];
-  let noProfit = 0;
-  for (let i = 0; i < prices.length; i++){
-    if (prices[i] > (prices[i + 1] === undefined ? 0 : prices[i + 1])) {
-      noProfit++
-    }
-  }
-  if(noProfit === prices.length) {
-    return 0
-  }
-  for (let i = 1; i < prices.length; i++){
+  let buyPrice = Infinity;
+  let maxPrice = 0;
+  for (let i = 0; i < prices.length; i++) { 
     if (prices[i] < buyPrice) {
       buyPrice = prices[i]
     }
-    else {
-     maximumProfit = Math.max(prices[i]-buyPrice,maximumProfit)
+    else if (prices[i] - buyPrice > maxPrice) {
+      maxPrice = prices[i]-buyPrice
     }
   }
-
-  return maximumProfit
-};
-
-console.log(maxProfit([7,1,5,3,6,4]))
+  return maxPrice
+}
