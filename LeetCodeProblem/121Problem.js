47cc2625ba995bf06334c1088901d00e5247cbb9
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-07-16 23:15:35 +0600

    solved the problem in brute force way

---
+++ LeetCodeProblem/121Problem.js
@@ -0,0 +1,26 @@
+// solved the problem brute force way
+var maxProfit = function (prices) {
+  let maximumProfit = 0;
+  let buyPrice = prices[0];
+  let noProfit = 0;
+  for (let i = 0; i < prices.length; i++){
+    if (prices[i] > (prices[i + 1] === undefined ? 0 : prices[i + 1])) {
+      noProfit++
+    }
+  }
+  if(noProfit === prices.length) {
+    return 0
+  }
+  for (let i = 1; i < prices.length; i++){
+    if (prices[i] < buyPrice) {
+      buyPrice = prices[i]
+    }
+    else {
+     maximumProfit = Math.max(prices[i]-buyPrice,maximumProfit)
+    }
+  }
+
+  return maximumProfit
+};
+
+console.log(maxProfit([7,1,5,3,6,4]))
