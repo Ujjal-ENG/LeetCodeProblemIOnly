9e6fc544fb2bbea42b1ac634c27d344a5d28581b
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-07-16 17:17:38 +0600

    solved the first test cased

--- test.js
+++ test.js
@@ -1,8 +1,15 @@
 var maxProfit = function (prices) {
   let maximumProfit = 0;
+  let count = 0;
   for (let i = 0; i < prices.length; i++){
-    if(i)
+    if (prices[i] > (prices[i + 1] === undefined ? 0 : prices[i + 1])) {
+      count++
+    }
   }
+  if (count === prices.length) {
+    return 0
+  }
+  return 1
 };
 
-console.log(maxProfit([7,1,5,3,6,4]))
+console.log(maxProfit([7,6,4,3,1]))
